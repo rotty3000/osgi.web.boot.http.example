71184acbba3e779a9624fb45f6ceca236e46ff31
Raymond Auge <raymond.auge@liferay.com>
2016-09-29 11:50:13 -0400

    [example] simple http example

---
+++ http.example/src/web/sample/SampleServlet.java
@@ -0,0 +1,33 @@
+package web.sample;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+@Component(
+	immediate = true,
+	property = {
+		HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN + "=/*"
+	},
+	service = Servlet.class
+)
+public class SampleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(
+			HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		response.getWriter().println("Hello World!");
+	}
+
+}
